278bbe685554f5b516db4f95195efabdd7855a2b
Flávio Alexandre <99499348+ClarkAshida@users.noreply.github.com>
2023-12-04 16:43:30 -0300

    Alteração no teste de controller de funcionário #passouTudo

--- funcionarioControllerTest.py
+++ funcionarioControllerTest.py
@@ -2,12 +2,10 @@ from controller.funcionarioController import CadastrarFuncionario, ExcluirFuncio
 
 if __name__ == '__main__':
     print('====Adicionar====')
-    #CadastrarFuncionario.cadastrar_funcionario("a", "b", "c", "d") #FEITO
-    #ListarFuncionario.listar_funcionarios() #FEITO
-    #BuscarFuncionario.buscar_funcionario_por_id(1) #FEITO
-    #AtualizarFuncionario.alterar_dados_funcionario(3, "nome", "cpf", "login", "senha")
-    #ListarFuncionario.listar_funcionarios()
-    #ExcluirFuncionario.excluir_funcionario(1) #FEITO
-    #ListarFuncionario.listar_funcionarios() #FEITO
-
-
+    CadastrarFuncionario.cadastrar_funcionario("a", "b", "c", "d") #FEITO
+    ListarFuncionario.listar_funcionarios() #FEITO
+    BuscarFuncionario.buscar_funcionario_por_id(1) #FEITO
+    AtualizarFuncionario.alterar_dados_funcionario(1, "nome", "cpf", "login", "senha") #FEITO
+    ListarFuncionario.listar_funcionarios() #FEITO
+    ExcluirFuncionario.excluir_funcionario(1) #FEITO
+    ListarFuncionario.listar_funcionarios() #FEITO
