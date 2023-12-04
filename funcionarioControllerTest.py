b4262b9e74a368413153d7627445881d6bd18fa8
Flávio Alexandre <99499348+ClarkAshida@users.noreply.github.com>
2023-12-04 16:29:29 -0300

    CRIAÇÃO DO TESTE CONTROLLER

---
+++ funcionarioControllerTest.py
@@ -0,0 +1,13 @@
+from controller.funcionarioController import CadastrarFuncionario, ExcluirFuncionario, AtualizarFuncionario, ListarFuncionario, BuscarFuncionario
+
+if __name__ == '__main__':
+    print('====Adicionar====')
+    #CadastrarFuncionario.cadastrar_funcionario("a", "b", "c", "d") #FEITO
+    #ListarFuncionario.listar_funcionarios() #FEITO
+    #BuscarFuncionario.buscar_funcionario_por_id(1) #FEITO
+    #AtualizarFuncionario.alterar_dados_funcionario(3, "nome", "cpf", "login", "senha")
+    #ListarFuncionario.listar_funcionarios()
+    #ExcluirFuncionario.excluir_funcionario(1) #FEITO
+    #ListarFuncionario.listar_funcionarios() #FEITO
+
+
